6d7250ced4c9c2102c2b1cf6337e2c2f5853d59f
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-04-29 09:37:16 -0700

    added background image to main and have empty Header, Footer, Main components imported to App.js and displaying on Live Server

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import Header from './Header/Header.js';
+import Footer from './Footer/Footer.js';
+import Main from './Main/Main.js';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header text="Welcome to Life on the Farm" />
+      <Main />
+      <Footer />
     </div>
   );
 }
---
+++ src/Main/Main.js
@@ -0,0 +1,7 @@
+import backgroundImg from '../background.png';
+
+import React from 'react';
+
+export default function Main() {
+  return <div>Main</div>;
+}
