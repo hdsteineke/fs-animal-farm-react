d2a75a24567ea1f243b55055c08af3f465a08dc9
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-04-29 10:00:46 -0700

    created data.js with array of animals and phrases

---
+++ src/data.js
@@ -0,0 +1,26 @@
+export const animals = [
+  {
+    name: 'cow',
+    says: 'moo',
+  },
+
+  {
+    name: 'goose',
+    says: 'honk',
+  },
+
+  {
+    name: 'pig',
+    says: 'oink',
+  },
+
+  {
+    name: 'sheep',
+    says: 'bah-ram-yew',
+  },
+
+  {
+    name: 'spider',
+    says: '...',
+  },
+];
